6cb1e8dc348fdeca881ea471f135cd6611d8c86a
Jeremy <brambleshadow4@gmail.com>
2021-05-19 19:38:51 -0500

    added spanish translation

--- i18n/es-ES/views/tokens.ts
+++ i18n/es-ES/views/tokens.ts
@@ -1,11 +1,14 @@
+// Thanks to Wild Thunder06 + Mysterous science for helping translate this
+
 export default {
 	"HomepageAboutTheGame": "Sobre el juego",
-	"HomepageCustomCards" : "Actualización de las cartas personalizadas",
-	"HomepageCustomCardsLearn" : "Aprenda a cargar sus propias cartas",
-	"HomepageHostGame" : "Crear Sala",
-	"HomepageSourceCode" : "Código fuente",
-	"HomepageSubmitBug" : "Enviar un fallo",
-	"HomepageFindPlayers" : "buscar jugadores",
+	"HomepageCustomCards": "Actualización de las cartas personalizadas",
+	"HomepageCustomCardsLearn" : "Aprende a subir tus propias cartas",
+	"HomepageHostGame": "Crear Sala",
+	"HomepageSourceCode": "Código fuente",
+	"HomepageSubmitBug": "Enviar un fallo",
+	"HomepageFindPlayers": "buscar jugadores",
+	"HomepageTranslatedBy": "Traducido por Wild Thunder06 + Mysterious Science", // "Translated by brambleshadow4" English version doesn't need this message, but other translations should have this
 
 
 	"LobbyJoinGame": "Unirse al juego",
@@ -17,7 +20,7 @@ export default {
 	"LobbyChooseName": "Escoja un nombre",
 	"LobbyJoin": "Unirse",
 	"LobbyStartGame": "Iniciar el juego",
-	"LobbyRuleEnforcement": "Reglas de juego",
+	"LobbyRuleEnforcement": "Reglas del juego",
 	"LobbyRulesTurns": "Turnos - Sólo puede jugar una persona a la vez",
 	"LobbyRulesSandbox": "Sandbox - Todos pueden jugar simultáneamente",
 	"LobbyOtherOptions": "Otras opciones",
@@ -31,6 +34,98 @@ export default {
 	"LobbyChooseFile": "Elige el archivo",
 	"LobbyWaiting": "Esperando a que el anfitrión inicie el juego.",
 	"LobbyGameInSession": "Juego en curso. La sala para este juego está cerrada",
-	"LobbyDiscordStartWarning": "Las Goals no se pondrán automáticamente en verde si se utiliza esta carta de start",
-	"LobbyUploadStartCardLocation": "Las cartas de Start cargadas se encuentran en la sección 'Cartas Start'"
+	"LobbyDiscordStartWarning": "Las Goals no se pondrán en verde automáticamente si se utiliza esta carta de start",
+	"LobbyUploadStartCardLocation": "Las cartas de Start cargadas se encuentran en la sección 'Cartas Start'",
+	"LobbyPlayerJoining": "Jugador uniéndose",
+	"LobbyMiniExpansions": "Mini expansiones",
+	"LobbyUploads": "Subidas",
+
+
+	"GameYourTurn": "Es tu turno",
+	"GamePlayerTurn": "Es el turno de {0}", // {0} is the player's name
+	"GameDisconnectTimeout": "Su turno terminará si no se conecta en <15s",
+	"GameEndMyTurnButton": "Terminar mi turno",
+	"GameEffectShippedWithEveryPony": "Este pony se shipea con todos los demás pony en el tablero.",
+	"GameEffectCopying": "Copiando a {0}", // {0} is the name of a card,
+	"GamePointsWithUnit": "{0}pts", // {0} is the number of points the player has. "points" should be shortened to something really short, a.k.a. "pts"
+
+
+	"PopupTitleSelectDisguise": "Seleccionar disfraz",
+	"PopupTitleSpecial": "Special",
+	"PopupTitleCopy": "Copy",
+	"PopupTitleGenderChange": "Gender Change",
+	"PopupTitleClone": "Clone",
+	"PopupTitleTimelineChange": "Timeline Change",
+	"PopupTitleKeywordChange": "Keyword Change",
+	"PopupTitleRaceChange": "Race Change",
+	"PopupTitleDiscardedPonies": "Ponis descartados",
+	"PopupTitleDiscardedShips": "Ships descartados",
+	"PopupTitleDiscardedGoals": "Goals descartados",
+	"PopupTitleHostSettings": "Ajuste de la sala",
+	"PopupTitleChoosePoints": "Elija la cantidad de puntos",
+
+	"PopupChooseDisguise": "Elige disfraz para el pony",
+	"PopupChoosePonyToCopy": "Choose a pony to copy", // REvisaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaar
+
+	"PopupChoosePrincess":  "Elige una nueva princesa",
+	"PopupChoosePonyToChangeGender": "Elige un pony para cambiar de género",
+	"PopupChoosePonyToShipWithEverypony": "Elige un pony para shipearlo con todos",
+	"PopupChooseClone": "Elige un poni para que cuente como dos",
+	"PopupChooseTimlinePony": "Elige un pony para que gane <br> el símbolo de time traveller",
+	"PopupChoosePonyForKeywords": "Elige un pony para que gane un keyword",
+	"PopupChoosePonyAndRace": "Elige un pony y selecciona su nueva raza",
+	"PopupChoosePonyRaceAndGender": "Elige un poni y selecciona su nueva raza/género",
+	"PopupTextPickAKeyword": "Elige un keyword",
+	"PopupTextWonGoals": "Goals ganados de {0}", // {0} is the player name
+	"PopupOtherButton": "Otro",
+
+	"HostSettingKeepLobbyOpen": "Dejar que los jugadores se unan a mitad del juego",
+	"HostSettingNewGameButton": "Nuevo juego",
+	"HostSettingNewGameText": "Finaliza el juego y devuelve a todos los jugadores a la sala de espera.",
+	"HostSettingKickPlayers": "Expulsar jugadores",
+	"HostSettingKickPlayersText": "Expulsar a un jugador lo saca del lobby y descarta todas las cartas que tenga.",
+	"HostSettingKickPlayerButton": "Expulsar {0}", // {0} is the player's name
+
+
+	"HelpTabQuickStart": "Inicio rápido",
+	"HelpTabQuickRules": "Reglas rápidas",
+	"HelpTabFullRules": "Reglas completas",
+	"HelpTabFAQ": "FAQ",
+	"HelpTabCardReference": "Referencias de cartas",
+
+	"PopupTextPonyCards": "Cartas de Pony",
+	"PopupTextShipCards": "Cartas de Ship",
+	"PopupTextGoalCards": "Cartas de Goal",
+
+	"PopupTitleCustomCards": "Cartas personalizadas",
+	"CustomCardsHeading": "Cartas personalizadas en uso",
+	"CustomCardsArt": "Arte por Arya The Editor",
+	"CustomCardsWarning": "Parece que el anfitrión ha subido cartas personalizadas para usar en este juego.<br>Ojalá tengan buen gusto",
+	"ButtonOkay": "Okay",
+
+
+	"QuickStartSwitchToDesktop": "Ver las instrucciones para ordenadores",
+	"QuickStartSwitchToMobile": "Ver instrucciones para dispositivos táctiles",
+
+	"QuickStartDesktop1": "Roba cartas de pony, de ship y de goal tocando en los mazos de la izquierda.",
+	"QuickStartDesktop2": "Mantenga pulsada la tecla <span class='key'>Shift</span> y pase el ratón por encima de una cartas para verla con más detalle.",
+	"QuickStartDesktop3": "Coloca las cartas al tablero arrastrándolas y soltándolas desde tu mano.",
+	"QuickStartDesktop4": "Descarta las cartas manteniendo pulsado <span class='key'>D</span> y haciendo clic en ellas.",
+	"QuickStartDesktop5": "Intercambia las cartas por el tablero arrastrándolas y soltándolas sobre otra carta",
+	
+	"QuickStartMobile1": "Roba cartas de pony, de ship y de goal tocando en los mazos de la izquierda.",
+	"QuickStartMobile2": "Mantenga pulsada una carta para verla con más detalle.",
+	"QuickStartMobile3": "Coloca las cartas en el tablero tocando una en tu mano y seleccionando un lugar en el tablero.",
+	"QuickStartMobile4": "Descarta cartas tocando una para seleccionarla y luego tocando la pila de descarte",
+	"QuickStartMobile5": "Intercambia las cartas del tablero tocando una, y luego tocando la carta con la que quieres cambiarla",
+	"QuickStartMobile6": "Baraja los mazos + descarte tocando y manteniendo la pila de descarte hasta que se baraje.",
+
+	// Todo translate the entire search bar
+	"SearchBarPlaceholder": "Buscar nombre, raza, género, etc.",
+
+
+	"CardSelectNone": "Ninguna",
+	"CardSelectSome": "Algunas",
+	"CardSelectAll": "Todas"
+
 }	
